12816f8e2cb278ac8e487aa6e763ce550e629a62
AbhiramiMurugan <105522566+AbhiramiMurugan@users.noreply.github.com>
2023-03-15 19:22:00 -0700

    Add files via upload

---
+++ AppiumDemo.java
@@ -0,0 +1,23 @@
+package appium;
+
+import java.io.File;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class AppiumDemo {
+public static void main(String[] args) {
+		
+	File f = new File("src");
+	File fs = new File(f,"ApiDemos.apk");
+	
+	DesiredCapabilities cap = new DesiredCapabilities();
+	cap.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator1");
+	cap.setCapability(MobileCapabilityType.APP,fs.getAbsolutePath());
+	cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+	AndroidDriver<MobileElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+}
+}
